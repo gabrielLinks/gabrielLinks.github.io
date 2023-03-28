c69fbd3c60ce3fc428c78deb4baf5853424635f1
gabriel <gabrielelizondo241998@gmail.com>
2023-03-27 23:43:26 -0300

    reto4

--- reto3.js
+++ reto3.js
@@ -58,16 +58,19 @@ creatarea.addEventListener('click',(e)=>{
     const h2 = document.createElement('h2');
     h2.textContent = texto1;
     h2.className="soyh";
+    localStorage.setItem("NombreTarea",texto1)
     const h3 = document.createElement('h3');
     h3.textContent = texto2;
     h3.className="soyh";
+    localStorage.setItem("TipoTarea",texto2)
     const h4 = document.createElement('h4');
     h4.textContent = texto3;
     h4.className="soyh";
+    localStorage.setItem("Descripcion",texto3)
     const h5 = document.createElement('h5');
     h5.textContent = texto4;
     h5.className="soyh";
-
+    localStorage.setItem("Prioridad",texto4)
     li.appendChild(h2);
     li.appendChild(h3);
     li.appendChild(h4);
@@ -97,10 +100,30 @@ function botonBorrar(){
 
 
 
+let perfilface = document.getElementById('perfilface')
+let botonPerfil = document.getElementById('botonPerfil')
+
+perfilface.classList.add('desactivar')
+botonPerfil.addEventListener('click',()=>{
+    
+    perfilface.classList.toggle('desactivar')
+
+})
 
 
 
+let tresbarras = document.getElementById('tresbarras')
+tresbarras.addEventListener('click',()=>{
+    alert('No hay polque')
+})
 
-//remove.('activar')
+let contraseña = document.getElementById('contraseña')
+let cuenta = document.getElementById('cuenta')
+let okayokay = document.getElementById('okayokay')
 
-
+okayokay.addEventListener('click',()=>{
+    localStorage.setItem("user",cuenta.value)
+    localStorage.setItem("contraseña",contraseña.value)
+    perfilface.classList.add('desactivar')
+    
+})
