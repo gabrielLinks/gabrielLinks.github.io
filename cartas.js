b631bb74b7984cb9aa9bcb49ba1a049ea26183b7
gabriel <gabrielelizondo241998@gmail.com>
2023-03-30 22:40:07 -0300

    reto4actulizacion

---
+++ cartas.js
@@ -0,0 +1,141 @@
+const miMazo = [
+    {
+    id:0,
+    imagen:'imagenesRet4/carta29.TheBeast.png',
+    nombre:'The Beast',
+    descrip:'La bestia te encontrara, no importa donde te escondas o que tanto huyas el rastro que dejas de miedo el lo encontrara',
+    ataque:350
+},
+{
+    id:1,
+    imagen:'imagenesReto4/carta2.png',
+    nombre:'La Torre',
+    descrip:'Esta carta Magica dorada te permite proteger a una de  tus cartas agregando puntos de ataque a la carta elegida',
+    ataque: 300
+},
+{
+    id:2,
+    imagen:'imagenesReto4/carta3.ElProtector.png',
+    nombre:'El Protector',
+    descrip:'Esta carta magica te permite proteger a una de tus cartas agregando puntos de ataque a la carta elegida ',
+    ataque:310
+},
+{
+    id:3,
+    imagen:'imagenesReto4/carta4.ElVerdugo.png',
+    nombre:'El Verdugo',
+    descrip:'Esta carta Especial tiene un rango de ataque elevado que ya que el verdugo puede lanzar su hacha',
+    ataque:490
+},
+{
+    id:4,
+    imagen:"imagenesReto4/carta7PoisonApple.png",
+    nombre:'Posison Apple',
+    descrip:'Esta carta Magica Especial envenena a todas las cartas del oponente',
+    ataque:100
+},
+{
+    id:5,
+    imagen:"imagenesReto4/carta8.Brujo.png",
+    nombre:'The Witcher',
+    descrip:'Esta carta Magica Especial, elimina una carta del oponente al azar mandandola al cementerio, sin importar el ataque que esta tenga ',
+    ataque:100
+},
+{
+    id:6,
+    imagen:"imagenesReto4/carta9.DarkTrunk.png",
+    nombre:'Dark Trunk',
+    descrip:'Esta carta anula cualquier ataque de las cartas especiales del oponente',
+    ataque:100
+},  
+{
+    id:7,
+    imagen:"imagenesReto4/carta10.DivineCreatures.png",
+    nombre:'Divine Creatures',
+    descrip:'El usuario de esta carta puede revivir una carta de su cementerio ',
+    ataque:100
+},
+{
+    id:8,
+    imagen:"imagenesReto4/carta11.Colossus.png",
+    nombre:'Colossus',
+    descrip:'Despertar a este coloso es el mas grande error que cometio tu oponente, este no se detendra hasta eliminar al menos dos cartas del oponente',
+    ataque:555
+},    {
+    id:9,
+    imagen:"imagenesReto4/carta14.SkullGhost.png",
+    nombre:'Skull Ghost',
+    descrip:'Este fantasma es inmune a cualquier ataque fisico, muy pocos puden sobrevivir al toparse con el',
+    ataque: 410
+},
+{
+    id:10,
+    imagen:"imagenesReto4/carta13.Canserbero.png",
+    nombre:'Canserbero',
+    descrip:'Este guardian se activa solamante si tiene una carta en tu cementerio',
+    ataque: 666
+},
+{
+    id:11,
+    imagen:"imagenesReto4/carta16_BonesWarrior.png",
+    nombre:'Bones Warrior',
+    descrip:'Este guerrero volvio a la vida gracias a la ayuda devina de los dioses. Cada golpes que acierte este guerrero aumentara su fuerza de ataque ',
+    ataque:777
+},
+{
+    id:12,
+    imagen:"imagenesReto4/carta15.Dionaea.png",
+    nombre:'Dionaea',
+    descrip:'Ataquela cuantas veces quieras por cada corte le saldran mas espinas y si cortas una de sus cabezas esta de duplicara',
+    ataque: 666
+},
+{
+    id:13,
+    imagen:"imagenesReto4/carta17.vishnu.png",
+    nombre:'Vishnu',
+    descrip:'Esta carta permite al que la posea, poder cambiar una cantar con el oponente al azar',
+    ataque:554
+},
+{
+    id:14,
+    imagen:"imagenesReto4/carta20.doppelganger.png",
+    nombre:'Doppelganger',
+    descrip:'Esta carta duplica los puntos de ataque de todas las cartas de tu mazo, si es enviada al cementerio sus efectos se mantiene ',
+    ataque:222
+},
+{
+    id:16,
+    imagen:"imagenesReto4/carta22.PunkFrog.png",
+    nombre:'Punk Frog',
+    descrip:'Esta rana punk por cada golpe que reciba lo devuelve el doble',
+    ataque: 333
+},
+{
+    id:17,
+    imagen:"imagenesReto4/carta31.BlockFace.png",
+    nombre:'BlockFace',
+    descrip:'Esta guerro tiene la habilida de reduci a la mitad el ataque de la carta que enfrente ',
+    ataque: 444
+},
+{
+    id:18,
+    imagen:"imagenesReto4/carta23.SkullKnight.png",
+    nombre:'Skull Knight',
+    descrip:'El caballero calabara te mandara a otra dimension si te interpones en su camino',
+    ataque:545
+},
+{
+    id:19,
+    imagen:"imagenesReto4/carta25.cyborg.png",
+    nombre:'Cyborg',
+    descrip:'Este guerror mitad hombre mitad metal en caso de fallar en su mision activara su autodestruccion genernando el mayor daño posible al oponente',
+    ataque:548
+},
+{
+    id:20,
+    imagen:"imagenesReto4/carta30.KingGhidorah.png",
+    nombre:'King Ghidorah',
+    descrip:'Si el Rey es despertado todo acabara',
+    ataque:999
+}
+];
---
+++ reto4.js
@@ -0,0 +1,126 @@
+let pantalla1 = document.getElementById('pantalla1')
+let jugador1 = document.getElementById('jugador1')
+let jugador2 = document.getElementById('jugador2')
+let mezclador= document.getElementById('mezclador')
+let pantalla2 = document.getElementById('pantalla2')
+let pantalla3 = document.getElementById('pantalla3')
+let hederloco = document.getElementById('hederloco')
+let carrousel =document.getElementById('carouselExampleCaptions')
+let jugadoresCuenta = document.getElementById('jugadores')
+let resultado= document.getElementById('resultado')
+let elem = 0;
+let player1=[]
+let player2 =[]
+let cartas= []
+
+
+
+pantalla2.style.display='none'
+pantalla3.style.display='none'
+
+mezclador.addEventListener('click',()=>{
+    if(jugador2.value.length >0 && jugador1.value.length>0){
+    pantalla1.style.display='none'
+    hederloco.style.display='none'
+    pantalla2.style.display = 'block'
+    setTimeout(tiempoCarga,3000)
+    iniciarJuego()
+    }else{
+        alert('Los datos ingresados no son validos')
+    }
+});
+
+
+function tiempoCarga(){
+    pantalla1.style.display='none';
+    pantalla2.style.display='none';
+    pantalla3.style.display='block'
+}
+
+function reset(){
+    jugador2.value =''
+    jugador1.value = ''
+}
+
+function volver(){
+pantalla3.style.display='none';
+pantalla1.style.display='block';
+    reset()
+}
+
+
+
+const iniciarJuego = ()=>{
+    const cartasJuego=[]
+
+    while(cartasJuego.length < 6){
+        random = Math.floor(Math.random()*miMazo.length);
+        if(cartasJuego.indexOf(miMazo[random])== -1){
+            cartasJuego.push(miMazo[random])
+        }
+    }
+    
+
+    let active =' '
+cartasJuego.forEach((miMazo, index) => { 
+        if(index == 0){
+            active= 'active'
+        }else{active = ''
+    }
+    carrousel.innerHTML += `<div class="carousel-item ${active}" class="org" data-id=${index + 1}>
+    <img src=${miMazo.imagen} class="img-carta" class="d-block w-100" alt="...">          
+    <h5>${miMazo.nombre}</h5>
+    <p>${miMazo.descrip}</p>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+    </button>`
+
+})
+    jugadoresCuenta.innerHTML= `<h3>Estas cartas son de ${jugador1.value}</h3>`
+
+while (player1.length < 3){
+    player1.push(cartasJuego.slice(0,3))
+}
+    console.log(player1)
+
+while(player2.length <3){
+    player2.push(cartasJuego.slice(3,6))
+}
+    console.log(player2)
+
+    const myCarousel = document.getElementById('carouselExampleCaptions')
+
+    myCarousel.addEventListener('slide.bs.carousel',event =>{
+
+    let elem = event.relatedTarget.getAttribute('data-id');
+    if (elem < 4){
+        jugadoresCuenta.innerHTML = `<h3>Estas cartas son de ${jugador1.value}</h3>`
+    } else {
+        jugadoresCuenta.innerHTML = `<h3>Estas cartas son de ${jugador2.value}</h3>`
+    }
+
+
+    })
+}
+
+const resultade = ()=> {
+    resultado.addEventListener('click',()=>{
+    pantalla3.style.display= 'none'
+    pantalla4.style.display='block'
+    })
+    function resultadox(){
+        if(cuenta % 2 ==0){
+            resultadox = true
+        }else{
+            resultadox = false
+        }
+    }
+}
+
+
